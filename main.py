ba4a51421c35849183601d7a512dddc583e69ca9
peerpressurre <gsophiay@gmail.com>
2022-12-10 10:35:15 +0200

    1f

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 
+try:
+    with open("text.txt", "w") as file:
+        file.write('Engineering fights going melee. Legendary like Tiger JK')
+
+    with open("text.txt", "r") as file:
+        for line in file:
+            print("Original text: ", line, end=" ")
+    print()
+    sevenlist = []
+    splitted = line.split(' ')
+    for item in splitted:
+        l = len(item)
+        #print(l, end=" ")
+        if l > 7:
+            sevenlist.append(item)
+            with open('seven.txt', 'w') as fle:
+                print(f"Words that contain 7 or more letters: {sevenlist}", file=fle)
+                print(f"Words that contain 7 or more letters: {item}")
+
+
+except Exception as ex:
+    print(f"Error: {ex}")
